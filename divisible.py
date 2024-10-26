77d2b2c74f3b9c6cf1ae37d0267036be56536b4a
MohammadA20 <mohamadadada123@gmail.com>
2024-10-26 21:27:56 +0500

    First Try

---
+++ divisible.py
@@ -0,0 +1,7 @@
+x = int(input("Please enter a number: "))
+
+for mohammad in range(1, x + 1):
+    if mohammad % 3 == 0 and mohammad % 5 == 0:
+        print(mohammad , "is divisible by 3 and 5")
+        
+    
